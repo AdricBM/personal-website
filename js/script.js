8438076ef49f0d877b0a621060de538cc24c0d45
Adric1 <aplanktin@gmail.com>
2021-12-01 20:14:48 +0200

    fixed projects carousel

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// nav bar scroll effect
 $(document).ready(function () {
   $(window).scroll(function () {
     if (this.scrollY > 20) {
@@ -9,9 +10,34 @@ $(document).ready(function () {
 
   // navbar menu button toggle
 
-  $('.menu-btn').click(function(){
-      $('.navbar .menu').toggleClass("active");
-      $('.menu-btn i').toggleClass("active");
-  })
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
+  //owl carousel slider
+
+  $(".slider").owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: true,
+    dots: false,
+    navText: [
+      "<i class='fa fa-caret-left'></i>",
+      "<i class='fa fa-caret-right'></i>",
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
 });
